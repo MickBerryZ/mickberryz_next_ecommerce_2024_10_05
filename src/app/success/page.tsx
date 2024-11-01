b78ccdddcc692823bc5cc56ed3b5a0820ab55dff
mickberryz <sa.thawatchai@gmail.com>
2024-11-01 00:48:28 +0000

    Checkout & Connect handleCheckout  @wix/redirects  [I don't pay wixPayment]

---
+++ src/app/success/page.tsx
@@ -0,0 +1,9 @@
+const SuccessPage = () => {
+  return (
+    <div className="">
+      <h1>SuccessPage</h1>
+    </div>
+  );
+};
+
+export default SuccessPage;
--- src/components/CartModel.tsx
+++ src/components/CartModel.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useCartStore } from "@/hooks/useCartStore";
 import { media as wixMedia } from "@wix/sdk";
 import { useWixClient } from "@/hooks/useWixClient";
+import { currentCart } from "@wix/ecom";
 
 const CartModel = () => {
   // TEMPORARY
@@ -11,6 +12,30 @@ const CartModel = () => {
 
   const wixClient = useWixClient();
   const { cart, isLoading, removeItem } = useCartStore();
+
+  const handleCheckout = async () => {
+    try {
+      const checkout =
+        await wixClient.currentCart.createCheckoutFromCurrentCart({
+          channelType: currentCart.ChannelType.WEB,
+        });
+
+      const { redirectSession } =
+        await wixClient.redirects.createRedirectSession({
+          ecomCheckout: { checkoutId: checkout.checkoutId },
+          callbacks: {
+            postFlowUrl: window.location.origin,
+            thankYouPageUrl: `${window.location.origin}/success`,
+          },
+        });
+
+      if (redirectSession?.fullUrl) {
+        window.location.href = redirectSession.fullUrl;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
@@ -91,6 +116,7 @@ const CartModel = () => {
               <button
                 className="rounded-md py-3 px-4 bg-black text-white disabled:cursor-not-allowed disabled:opacity-75"
                 disabled={isLoading}
+                onClick={handleCheckout}
               >
                 Checkout
               </button>
