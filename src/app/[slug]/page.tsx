f60b95d831729f0df8ceb027351342f6fe93a75f
mickberryz <sa.thawatchai@gmail.com>
2024-12-08 17:32:42 +0000

    productId={product._id as string}

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -54,7 +54,7 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
           />
         ) : (
           <Add
-            productId={product._id ?? ""}
+            productId={product._id as string}
             variantId="00000000-0000-0000-0000-000000000000"
             stockNumber={product.stock?.quantity || 0}
           />
